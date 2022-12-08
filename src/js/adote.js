febdf7c5ac2f6d10341e78e73defc5593f972255
V-Carvalho <vinicius.a.carvalho@outlook.com>
2022-12-08 13:58:32 -0300

    Otimização da função que carrega o arquivo com os dados dos animais

--- src/js/adote.js
+++ src/js/adote.js
@@ -3,43 +3,36 @@ API CATS = https://cataas.com/
 API DOGS = https://dog.ceo/dog-api/
 */
 
-async function loadData(type) {
-  // Carregando o arquivo com os dados dos animais
-  const DATA = await fetch("../dao/data.json");
-  let animalData = await DATA.json();
+let rawData
+let animalData
 
-  switch (type) {
-    case "all":
-      getAllAnimals(animalData);
-      break;
-    case "dog":
-      getFilteredSortedData(animalData, "dog");
-      break;
-    case "cat":
-      getFilteredSortedData(animalData, "cat");
-      break;
-  }
+async function loadData() {
+  // Carregando o arquivo com os dados dos animais
+  rawData = await fetch("../dao/data.json");
+  animalData = await rawData.json();
+
+  getAllAnimals();
 }
 
-async function getAllAnimals(data) {
+function getAllAnimals() {
   // Limpando a lista
   document.getElementById("listAnimals").innerHTML = "";
 
-  for (let i = 0; i < data.length; i++) {
-    // Criando o elemento que vai ser inserido na lista
+  for (let i = 0; i < animalData.length; i++) {
+    // Criando o elemento que vai ser inserido na lista e atribuindo na variavel
     let animal = document.createElement("li");
 
     // Formatando o HTML que vai ser inserido na lista (ul)
     animal.innerHTML = `<li class="item-list-animals">
             <div class="card">
               <img
-                src=${data[i]["animalPhotoUrl"]}
+                src=${animalData[i]["animalPhotoUrl"]}
                 class="animal-picture"
               />
               <div class="animal-details">
-                <h4><b>${data[i]["animalName"]}</b></h4>
-                <p>${data[i]["age"]}</p>
-                <p>${data[i]["gender"]}</p>
+                <h4><b>${animalData[i]["animalName"]}</b></h4>
+                <p>${animalData[i]["age"]}</p>
+                <p>${animalData[i]["gender"]}</p>
               </div>
             </div>
           </li>`;
@@ -49,19 +42,19 @@ async function getAllAnimals(data) {
   }
 }
 
-async function getFilteredSortedData(data, type) {
+async function getDataAnimalsFilteredSorted(type) {
   // Limpando a lista
   document.getElementById("listAnimals").innerHTML = "";
 
   // Filtrando e sorteando os dados q serão inseridos na lista
-  const filteredSortedData = await data
+  const filteredSortedData = await animalData
     // Filtrando os animais conforme o tipo (gato ou cachorro)
     .filter((pet) => pet.tag == type)
     //Math.random() - 0.5 gera número aleatório que pode ser positivo ou negativo, então a função de classificação reordena os elementos aleatoriamente.
     .sort(() => Math.random() - 0.5) 
 
   for (let i = 0; i < filteredSortedData.length; i++) {
-    // Criando o elemento que vai ser inserido na lista
+    // Criando o elemento que vai ser inserido na lista e atribuindo na variavel
     let animal = document.createElement("li");
 
     // Formatando o HTML que vai ser inserido na lista (ul)
